5285263e2ede41c572190d8fd488b47a3c7758a7
simoncek <simon.cek94@gmail.com>
2020-07-01 21:02:03 +0700

    Swagger added basepackage to scan for controller

--- src/main/java/com/strike/strijkatelier/config/SwaggerConfig.java
+++ src/main/java/com/strike/strijkatelier/config/SwaggerConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
@@ -36,6 +37,7 @@ public class SwaggerConfig {
                 .groupName("Strijkatelier API")
                 .apiInfo(apiInfo())
                 .select()
+                .apis(RequestHandlerSelectors.basePackage( "com.strike.strijkatelier.controller" ))
                 .paths(PathSelectors.any())
                 .build()
                 .forCodeGeneration(true)
