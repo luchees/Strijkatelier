a6305d071282600910c3bb5a1c8445db519fad77
vand149 <lucas.vandenabbeele@cloudway.be>
2020-07-01 18:35:39 +0200

    added BucketController get active buckets

--- src/main/java/com/strike/strijkatelier/repository/BucketRepository.java
+++ src/main/java/com/strike/strijkatelier/repository/BucketRepository.java
@@ -4,7 +4,12 @@ import com.strike.strijkatelier.domain.entity.Bucket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface BucketRepository extends JpaRepository<Bucket, Long> {
 
+    List<Bucket> getBucketsByActive(Boolean active);
+    List<Bucket> getBucketsByActiveAndStartDateTime(Boolean active, Date startDateTime);
 }
